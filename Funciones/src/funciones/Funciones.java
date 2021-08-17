b278fc5fa504e2055dd6c4dcd241fda45282b6a8
damianbenitez89 <78812666+damianbenitez89@users.noreply.github.com>
2021-08-16 21:12:38 -0300

    funciones
    ejemplo de funciones

---
+++ Funciones/src/funciones/Funciones.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author CamiDamiAmelie
+ */
+public class Funciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese tu nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.println("tu nombre es "+ nombre);
+        
+        
+    }
+    
+}
