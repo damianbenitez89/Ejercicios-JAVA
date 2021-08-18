f6b0708272f39167b34f2dc758e162511a41bd53
damianbenitez89 <78812666+damianbenitez89@users.noreply.github.com>
2021-08-18 17:35:58 -0300

    GestionDeClientes

---
+++ Gestion_de_Clientes/src/gestion_de_clientes/Gestion_de_Clientes.java
@@ -0,0 +1,22 @@
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion_de_clientes;
+
+/**
+ *
+ * @author CamiDamiAmelie
+ */
+public class Gestion_de_Clientes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+    
+}
