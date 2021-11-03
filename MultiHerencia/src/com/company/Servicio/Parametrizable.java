a0c602d78e0105dc95581c86e6ced85b9dfd4e9d
damianbenitez89 <78812666+damianbenitez89@users.noreply.github.com>
2021-11-03 10:49:36 -0300

    correccion

--- MultiHerencia/src/com/company/Servicio/Parametrizable.java
+++
@@ -1,9 +0,0 @@
-package com.company.Servicio;
-
-public interface Parametrizable {
-    double IVA=21;
-    double CIF=0.06;
-    boolean verificarExistencia(int cant);
-    void actualizarStock (int cant);
-
-}
